50a24dfa940846402f9dfcfb00a2d9005ccf4cf6
John Donaldson <johndon@phhp.ufl.edu>
2025-08-12 13:48:25 -0400

    fixed path to shibalike blade file

--- src/StudentAffairsUwm/Shibboleth/ShibbolethServiceProvider.php
+++ src/StudentAffairsUwm/Shibboleth/ShibbolethServiceProvider.php
@@ -20,7 +20,7 @@ class ShibbolethServiceProvider extends ServiceProvider
 
         if (config('shibboleth.emulate_idp') === true) {
             $this->publishes([
-                __DIR__ . '/../../resources/views/Shibalike/' => resource_path('views/vendor/shibalike'),
+                __DIR__ . '/../../resources/views/shibalike/' => resource_path('views/vendor/shibalike'),
             ]);
 
             $this->loadRoutesFrom(__DIR__ . '/../../routes/shibalike.php');
